af8f3a90475bb8100752a92580de9269cb67d2b2
Prince Oncada <prince.oncada@gmail.com>
2023-10-22 03:41:19 +0800

    init commit

--- data_retrieval/test.py
+++ data_retrieval/test.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
 
 def main():
+    service = Service('./chromedriver.exe')
     # Initialize the WebDriver (you may need to specify the path to your WebDriver executable)
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(service=service)
 
     # Open a website
     driver.get("https://www.google.com/")
